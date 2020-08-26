13cd2a5e17c4da3a1c6aaa29c84b23ffd3462ae4
smsievers <seth_sievers@yahoo.com>
2020-08-26 14:25:15 -0500

    before deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,30 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :movies
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults:{format: :json} do
+    namespace :v1 do
+      namespace :users do
+        post :login
+        post :create
+        delete :logout
+        get :me
+      end
+
+      namespace :movies do
+        get :index
+        get :show
+        post :create
+        patch :update
+        delete :destroy
+        get :get_upload_credentials
+      end
+
+      namespace :reviews do
+        get :index
+        get :show
+        post :create
+        patch :update
+        delete :destroy
+      end
+
+    end
+  end
 end
