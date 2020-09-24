4f8ee67cf7651faf5eaac97e3bb4a8b2a5c52337
smsievers <seth_sievers@yahoo.com>
2020-09-24 14:48:16 -0500

    learning to type

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,5 +1,5 @@
 class Api::v1::MoviesController < ApplicationController
-    before_action :set_movie, only [:show, :update, :destroy]
+    before_action :set_movie, only: [:show, :update, :destroy]
     skip_before_action :authenticate, only: [:index, :show]
 
     def index
